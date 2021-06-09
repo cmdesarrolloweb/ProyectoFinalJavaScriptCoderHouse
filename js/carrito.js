952478e7c1088c77d7e9074970099f8da252b716
manteniendowordpress <info@manteniendowordpress.com>
2021-06-08 21:31:57 -0300

    agrego imagenes e itero los productos desde el archivo productos javascript

---
+++ js/carrito.js
@@ -0,0 +1,11 @@
+class Carrito {
+    //Añadir el producto al carrito
+    comprarProducto(e) {
+        e.preventDefault();
+        if(e.target.classList.contains('agregar-carrito')) {
+            const producto = e.target.parentElement.parentElement;
+            // this.leerDatosProducto(producto);
+            console.log(producto);
+        }
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,101 +1,88 @@
-// Simulador carrito de pedido/futuro carro de compras.
-class Producto {
-    constructor(nombre, precio) {
-        this.nombre = nombre.toUpperCase();
-        this.precio = parseFloat(precio);
-    }
-    
-}
-const prod1 = new Producto("Yerba Mañanita", "125");
-const prod2 = new Producto("Azúcar Chango", "100");
-const prod3 = new Producto("Café Dolca", "200");
-const prod4 = new Producto("Galletitas Pepitos", "80");
-
 //Array
-const listaProductos = [];
-listaProductos.push(prod1);
-listaProductos.push(prod2);
-listaProductos.push(prod3);
-listaProductos.push(prod4);
+// const listaProductos = [];
+// listaProductos.push(prod1);
+// listaProductos.push(prod2);
+// listaProductos.push(prod3);
+// listaProductos.push(prod4);
 
-function verListaProductos() {
-    let total = 0;
-    for (elemento of listaProductos) {
-        console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
-        //total = total + elemento.precio;
-        total += elemento.precio;
-    }
-    console.log("Son " + listaProductos.length + " productos\n  Total de la compra: $" + total);
-}
+// function verListaProductos() {
+//     let total = 0;
+//     for (elemento of productos) {
+//         console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
+//         //total = total + elemento.precio;
+//         total += elemento.precio;
+//     }
+//     console.log("Son " + productos.length + " productos\n  Total de la compra: $" + total);
+// }
 
-//Ordeno por precio de Menor a mayor
-function ordenarPorPrecioAsc() {
-    for (elemento of listaProductos.sort((a, b) => a.precio - b.precio)) {
-        console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
-    }
-}
+// //Ordeno por precio de Menor a mayor
+// function ordenarPorPrecioAsc() {
+//     for (elemento of productos.sort((a, b) => a.precio - b.precio)) {
+//         console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
+//     }
+// }
 
-//Ordeno por precio de Mayor a menor
-function ordenarPorPrecioDesc() {
-    for (elemento of listaProductos.sort((a, b) => b.precio - a.precio)) {
-        console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
-    }
-}
+// //Ordeno por precio de Mayor a menor
+// function ordenarPorPrecioDesc() {
+//     for (elemento of productos.sort((a, b) => b.precio - a.precio)) {
+//         console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
+//     }
+// }
 
-//Ordeno alfabéticamente Ascendente
-function ordenarAlfabetAz() {
-    for (elemento of listaProductos.sort((a, b) =>
-    a.nombre.toUpperCase() > b.nombre.toUpperCase() ? 1 :
-    a.nombre.toUpperCase() < b.nombre.toUpperCase() ? -1:
-    0
-  )) {
-        console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
-    }
-}
+// //Ordeno alfabéticamente Ascendente
+// function ordenarAlfabetAz() {
+//     for (elemento of productos.sort((a, b) =>
+//     a.nombre.toUpperCase() > b.nombre.toUpperCase() ? 1 :
+//     a.nombre.toUpperCase() < b.nombre.toUpperCase() ? -1:
+//     0
+//   )) {
+//         console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
+//     }
+// }
 
-//Ordeno alfabéticamente Descendente
-function ordenarAlfabetZa() {
-    for (elemento of listaProductos.reverse((a, b) =>
-    a.nombre.toUpperCase() > b.nombre.toUpperCase() ? 1 :
-    a.nombre.toUpperCase() < b.nombre.toUpperCase() ? -1:
-    0
-  )) {
-        console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
-    }
-}
+// //Ordeno alfabéticamente Descendente
+// function ordenarAlfabetZa() {
+//     for (elemento of productos.reverse((a, b) =>
+//     a.nombre.toUpperCase() > b.nombre.toUpperCase() ? 1 :
+//     a.nombre.toUpperCase() < b.nombre.toUpperCase() ? -1:
+//     0
+//   )) {
+//         console.log("Nombre: " + elemento.nombre + " Precio: " + elemento.precio);
+//     }
+// }
 
-//Función subTotal()
-function sumaProductos(item1, item2, item3, item4){
-    var montoTotal = item1 + item2 + item3 + item4;
-    return montoTotal;   
-}
-function calculaIva(subTotal) {
-    totalConIva = subTotal * 0.21;
-    return totalConIva;
-}
+// //Función subTotal()
+// function sumaProductos(item1, item2, item3, item4){
+//     var montoTotal = item1 + item2 + item3 + item4;
+//     return montoTotal;   
+// }
+// function calculaIva(subTotal) {
+//     totalConIva = subTotal * 0.21;
+//     return totalConIva;
+// }
 
-var subTotal = sumaProductos(prod1.precio, prod2.precio, prod3.precio, prod4.precio);
-console.log(subTotal);
+// var subTotal = sumaProductos(prod1.precio, prod2.precio, prod3.precio, prod4.precio);
+// console.log(subTotal);
 
-var iva = calculaIva(subTotal);
-console.log(iva);
+// var iva = calculaIva(subTotal);
+// console.log(iva);
 
-var total = subTotal + iva;
+// var total = subTotal + iva;
 
-//FUNCION FLECHA
-var finalizarCompra = () => {
-let nombre = prompt("Ingresa nombre:");
-let apellido = prompt("Ingresa apellido:");
-console.log("Hola " + nombre + " " + apellido + " has seleccionado 4 productos " 
-    + "\n"
-    + prod1.nombre + " $"+prod1.precio + "\n"
-    + prod2.nombre + " $"+prod2.precio + "\n"
-    + prod3.nombre + " $"+prod3.precio + "\n"
-    + prod4.nombre + " $"+prod4.precio + "\n"
-    + "------------------------" + "\n"
-    + "Subtotal: " + "$"+subTotal + "\n"
-    + "------------------------" + "\n"
-    + "IVA: " + "$"+iva + "\n"
-    + "------------------------" + "\n"
-    + "Total: " + "$"+total);
-}
+// //FUNCION FLECHA
+// var finalizarCompra = () => {
+// let nombre = prompt("Ingresa nombre:");
+// let apellido = prompt("Ingresa apellido:");
+// console.log("Hola " + nombre + " " + apellido + " has seleccionado 4 productos " 
+//     + "\n"
+//     + prod1.nombre + " $"+prod1.precio + "\n"
+//     + prod2.nombre + " $"+prod2.precio + "\n"
+//     + prod3.nombre + " $"+prod3.precio + "\n"
+//     + prod4.nombre + " $"+prod4.precio + "\n"
+//     + "------------------------" + "\n"
+//     + "Subtotal: " + "$"+subTotal + "\n"
+//     + "------------------------" + "\n"
+//     + "IVA: " + "$"+iva + "\n"
+//     + "------------------------" + "\n"
+//     + "Total: " + "$"+total);
+// }
