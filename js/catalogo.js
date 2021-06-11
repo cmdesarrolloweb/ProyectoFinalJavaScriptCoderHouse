f3bea848544a7414bc561df7fb5b5a9eac338b20
manteniendowordpress <info@manteniendowordpress.com>
2021-06-11 12:29:33 -0300

    Hago el intento de que me agregue productos al carrito, sin exito

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,17 +1,15 @@
 //Agrego los productos en la página catalogo.html
 for (const producto of listaProductos) {
-    let contenedor = document.createElement("div");
-    contenedor.setAttribute("class", "col-12 col-sm-4 mb-1 mb-md-4");
+    let contenedorProductos = document.createElement("div");
+    contenedorProductos.setAttribute("class", "col-12 col-sm-4 mb-1 mb-md-4");
     //Definimos el innerHTML del elemento con una plantilla de texto
-    contenedor.innerHTML = `
+    contenedorProductos.innerHTML = `
                         <div class="card rounded-4" id="producto${producto.id}">
                             <div class="card-body text-center d-flex
                                 d-md-block">
                                 <div class="col-2 col-md-12">
-                                    <a href="images/productos/${producto.imagen}" data-lightbox="producto${producto.id}"><img
-                                            src="images/productos/${producto.imagen}"
-                                            alt="Imagen Producto01"
-                                            width="100%"></a>
+                                    <a href="images/productos/${producto.imagen}" data-lightbox="producto${producto.id}">
+                                    <img src="images/productos/${producto.imagen}" alt="Imagen Producto01" width="100%"></a>
                                 </div>
                                 <div class="col-6 col-md-12">
                                     <h5 class="card-title fw-6 mt-1
@@ -25,7 +23,7 @@ for (const producto of listaProductos) {
                                         data-bs-target=""
                                         class="agregar-carrito d-flex d-md-block
                                         card-link btn btn-primario
-                                        col-12 mx-auto" data-id="producto${producto.id}">
+                                        col-12 mx-auto" id="boton${producto.id}">
                                         <span
                                             class="navbar-tool-icon
                                             shop-button
@@ -35,6 +33,11 @@ for (const producto of listaProductos) {
                                     </a>
                                 </div>
                             </div>
-                        </div>`;
-    document.getElementById("listar-productos").appendChild(contenedor);
+                        </div>`//el boton toma el id para hacer unico a ese boton, por eso se pone dentro del id y no fuera, no quiero tomar un dato para le html solo para hacer unico el boton.
+                        document.getElementById("listar-productos").appendChild(contenedorProductos);
+                        let boton = document.getElementById(`boton${producto.id}`) // tomo todo dentro de boton para que sea unico
+                        
+                        boton.addEventListener('click', ()=>{
+                            agregarAlCarrito(producto.id)
+                         });
 }
--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const producto6 = new Producto(6, "Azúcar Ledesma 1kg", 83.25, "Azucar-Ledesma-
 
 const listaProductos = [producto1, producto2, producto3, producto4, producto5, producto6];
 
-const listaDeCarrito = [];
+const carroDeCompras = [];
 
 const contenedorProductos = document.getElementById('listar-productos');
 const contenedorCarrito = document.getElementById('carrito-contenedor');
@@ -26,15 +26,96 @@ const contenedorCarrito = document.getElementById('carrito-contenedor');
 const contadorCarrito = document.getElementById('contadorCarrito');
 const precioTotal = document.getElementById('precioTotal');
 
-function agregarAListaCarrito() {
-    let productoAgregar = stockProductos.filter((el) => el.id == id)[0]
-    carritoDeCompras.push(productoAgregar)
+//Agrego los productos en la página catalogo.html
+for (const producto of listaProductos) {
+    let contenedorProductos = document.createElement("div");
+    contenedorProductos.setAttribute("class", "col-12 col-sm-4 mb-1 mb-md-4");
+    //Definimos el innerHTML del elemento con una plantilla de texto
+    contenedorProductos.innerHTML = `
+                        <div class="card rounded-4" id="producto${producto.id}">
+                            <div class="card-body text-center d-flex
+                                d-md-block">
+                                <div class="col-2 col-md-12">
+                                    <a href="images/productos/${producto.imagen}" data-lightbox="producto${producto.id}">
+                                    <img src="images/productos/${producto.imagen}" alt="Imagen Producto01" width="100%"></a>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    <h5 class="card-title fw-6 mt-1
+                                        mb-1 mt-lg-3 mb-lg-3 ps-2 px-lg-3
+                                        text-start text-md-center">${producto.nombre}</h5>
+                                </div>
+                                <div class="col-4 col-md-12">
+                                    <p class="card-text mb-1 mb-lg-4
+                                        fw-9">$ ${producto.precio}</p>
+                                    <a onclick="agregarAListaCarrito()" data-bs-toggle=""
+                                        data-bs-target=""
+                                        class="agregar-carrito d-flex d-md-block
+                                        card-link btn btn-primario
+                                        col-12 mx-auto" id="boton${producto.id}">
+                                        <span
+                                            class="navbar-tool-icon
+                                            shop-button
+                                            material-icons
+                                            align-middle">shopping_cart</span>&nbsp;<span
+                                            class="align-middle">AGREGAR</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>`//el boton toma el id para hacer unico a ese boton, por eso se pone dentro del id y no fuera, no quiero tomar un dato para le html solo para hacer unico el boton.
+                        document.getElementById("listar-productos").appendChild(contenedorProductos);
+                        let boton = document.getElementById(`boton${producto.id}`) // tomo todo dentro de boton para que sea unico
+                        
+                        boton.addEventListener('click', ()=>{
+                            agregarAlCarrito(producto.id)
+                         });
+}
+
+function agregarAListaCarrito(id) {
+    let productoAgregar = listaProductos.filter((el) => el.id == id)[0]
+    carroDeCompras.push(productoAgregar)
     actualizarCarrito() // ver esta funcion a lo ultimo 
     
 
     let div = document.createElement('div')
     div.classList.add('productoEnCarrito')
     div.innerHTML = `
+        <div class="item-lista-productos">
+            <div class="item-lista-vista">
+                <div class="item-carro-producto">
+                    <h5 class="item-nombre-producto">${productoAgregar.nombre}</h5>
+                    <div class="item-carro-detalle">
+                        <div class="item-carro-detalle-left">
+                            <div class="imgContenedor producto-img imgContenedor-hover">
+                            <img src="images/productos/${productoAgregar.imagen}" alt="Imagen Producto" width="100%">
+                            </div>
+                            <div class="btn-eliminar-carro-item">
+                                <button class="btn btn-eliminar font-semibold ml-2">ELIMINAR</button>
+                            </div>
+                        </div>
+                        <div class="item-carro-detalle-right">
+                            <div class="precio-contenedor">
+                                <h5 class="precio-descriptcion">Subtotal:</h5>
+                                <h5 class="precio-numero">$${productoAgregar.precio}</h5>
+                            </div>
+                            <div class="monto-input-contenedor">
+                                <div>
+                                    <p class="monto-texto">Cantidad</p>
+                                    <div class="numero-input inline-flex">
+                                        <button id="disminuir" type="button" onclick="disminuir()" class="btn-less input-number-button-less">
+                                            <i class="vs-icon notranslate icon-scale material-icons null">remove</i>
+                                        </button>
+                                        <input id="cantidad" type="number" min="0" class="input-number-input" style="width: 9.1px;">
+                                        <button id="aumentar" type="button" onclick="aumentar()" class="btn-plus input-number-button-plus">
+                                            <i class="vs-icon notranslate icon-scale material-icons null">add</i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <p>${productoAgregar.nombre}</p>
         <p>Precio: $${productoAgregar.precio}</p>
         <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
@@ -50,19 +131,36 @@ function agregarAListaCarrito() {
 }
 
 function actualizarCarrito() {
-    contadorCarrito.innerText = carritoDeCompras.length
-    precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
+    contadorCarrito.innerText = carroDeCompras.length
+    precioTotal.innerText = carroDeCompras.reduce((acc, el) => acc + el.precio, 0)
 }
 
 function verListaCarrito() {
     let total = 0;
-    for (producto of listaDeCarrito) {
+    for (producto of carroDeCompras) {
         console.log("ID: " + producto.id + " " + producto.nombre);
         total += producto.precio;
         //tambien se puede utilizar asi:
         //total = total + producto.precio;
     }
-    console.log("Son " + listaDeCarrito.length + " productos\n Total de la compra: $" + total);
+    console.log("Son " + carroDeCompras.length + " productos\n Total de la compra: $" + total);
+}
+
+//Funciones para aumentar o disminuir productos
+var inicio = 0; //se inicializa una variable en 0
+
+function aumentar(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
+
+var cantidad = document.getElementById('cantidad').value = ++inicio; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
+}
+
+function disminuir(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
+
+var cantidad = document.getElementById('cantidad').value = --inicio; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+
+if(cantidad > 0){
+
+}
 }
 
 
